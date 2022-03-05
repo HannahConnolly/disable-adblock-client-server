900b269b046d4ea3b116910aa834c7024212475f
HannahConnolly <connollyhannah93@gmail.com>
2022-03-05 00:45:32 -0700

    testing

--- disable-adblock-server/src/index.js
+++ disable-adblock-server/src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = 5555;
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,28 +28,44 @@ app.get('/', (req, res) => {
 });
 
 app.get('/status', (req, res) => {
-  getStatusFromConsole().then((status) => {
+  // getStatusFromConsole().then((status) => {
+  //   console.log('output: ' + status);
+  //   if (status.includes('Pi-hole blocking is enabled')) res.status(200);
+  //   res.send(status);
+  // });
+  lsExample().then((status) => {
     console.log('output: ' + status);
-    if (status.includes('Pi-hole blocking is enabled')) res.status(200);
-    res.send(status);
   });
 });
 
-async function getStatusFromConsole() {
-  let output;
-  await exec('pihole status', (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(stdout);
-    output = stdout;
-  });
-  return output;
+// async function getStatusFromConsole() {
+//   let output;
+//   await exec('pihole status', (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(`error: ${error.message}`);
+//       return;
+//     }
+//     if (stderr) {
+//       console.log(`stderr: ${stderr}`);
+//       return;
+//     }
+//     console.log(stdout);
+//     output = stdout;
+//   });
+//   return output;
+// }
+
+async function lsExample() {
+  const out = 'test';
+  try {
+    const { stdout, stderr } = await exec('pihole status');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    out = stdout;
+  } catch (e) {
+    console.error(e); // should contain code (exit code) and signal (that caused the termination).
+  }
+  return stdout;
 }
 
 buildDisableString = (input) => {
